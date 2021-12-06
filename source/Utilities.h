422608773754517f119d515f4b8b3d0962b1f921
Luciano-Bercini <lucianobercini7@gmail.com>
2021-12-06 20:24:01 +0100

    Improvements.

--- source/Utilities.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILITIES_H
-#define UTILITIES_H
-
-char *rand_alphanumID(char *buffer, size_t size);
-int strcontained(const char *str, const char **haystack, int haysize);
-uint uintcontained(const uint num, const uint *nums, int nums_size);
-#endif
